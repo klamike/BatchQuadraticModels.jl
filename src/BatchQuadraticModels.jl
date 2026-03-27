3075303fe5abaf8c1970e17d90d2ccee34a35f69
Klamkin, Michael <mklamkin3@gatech.edu>
2026-03-27 17:41:06 -0400

    provide max, min, sum

--- src/BatchQuadraticModels.jl
+++ src/BatchQuadraticModels.jl
@@ -16,10 +16,13 @@ abstract type AbstractSparseOperator{T} <: AbstractMatrix{T} end
 function gpu_operator end
 function operator_sparse_matrix end
 function batch_mapreduce! end
+function batch_maximum! end
+function batch_minimum! end
+function batch_sum! end
 
 export ObjRHSBatchQuadraticModel, BatchQuadraticModel
 export BatchSparseOp, batch_spmv!, _batch_spmv_impl!, _build_op
-export batch_mapreduce!
+export batch_mapreduce!, batch_maximum!, batch_minimum!, batch_sum!
 
 include("batch_mapreduce.jl")
 include("batch_spmv.jl")
--- src/batch_mapreduce.jl
+++ src/batch_mapreduce.jl
@@ -1,2 +1,9 @@
 batch_mapreduce!(f, op, neutral, out::AbstractMatrix, srcs::AbstractMatrix...) =
   (out .= mapreduce(f, op, srcs...; dims = 1, init = neutral))
+
+batch_maximum!(out::AbstractMatrix{T}, src::AbstractMatrix{T}) where {T} =
+  batch_mapreduce!(identity, max, typemin(T), out, src)
+batch_minimum!(out::AbstractMatrix{T}, src::AbstractMatrix{T}) where {T} =
+  batch_mapreduce!(identity, min, typemax(T), out, src)
+batch_sum!(out::AbstractMatrix{T}, src::AbstractMatrix{T}) where {T} =
+  batch_mapreduce!(identity, +, zero(T), out, src)
